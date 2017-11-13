7472bed2411502130ec12e2bb1ce0a3fbbcde1b3
aaron <wubo2@putao.com>
2017-11-13 14:15:21 +0800

    application version

--- Base/Application.php
+++ Base/Application.php
@@ -6,5 +6,10 @@ use Vivid\DI\ServiceLocator;
 
 class Application extends ServiceLocator
 {
+    const VERSION = '0.0.1';
 
+    public function version()
+    {
+        return static::VERSION;
+    }
 }
